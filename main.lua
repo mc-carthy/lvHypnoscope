1b7f18e7d417a993289d3ac40229622c8fea1774
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-16 13:49:01 +0000

    Create slime Entity

--- main.lua
+++ main.lua
@@ -2,10 +2,15 @@ local Entity = require("src.logic.Entity")
 local Sprite = require("src.graphics.Sprite")
 
 local player
+local playerSprite
+local slime
+local slimeSprite
 
 function love.load()
     playerSprite = Sprite.create("assets/sprites/adventurer.png", 0, 0, 4, 4)
     player = Entity.create(playerSprite, 50, 50, 100)
+    slimeSprite = Sprite.create("assets/sprites/slime.png", 0, 0, 4, 4)
+    slime = Entity.create(slimeSprite, 250, 50, 100)
 end
 
 function love.update(dt)
@@ -13,8 +18,10 @@ function love.update(dt)
         love.event.quit()
     end
     player:update(dt)
+    slime:update(dt)
 end
 
 function love.draw()
     player:draw()
+    slime:draw()
 end
