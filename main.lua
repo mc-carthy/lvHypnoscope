3e935d986d7deb418f346661272677c46d8e0ee9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-16 11:28:34 +0000

    Init repo

---
+++ main.lua
@@ -0,0 +1,12 @@
+love.load = function()
+
+end
+
+love.update = function()
+
+end
+
+love.draw = function ()
+    love.graphics.setColor(191, 0, 0, 255)
+    love.graphics.rectangle("fill", 0, 0, 100, 100)
+end
