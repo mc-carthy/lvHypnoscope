8b1949b4d06c7efd51a6e53a7179b9893f9afe0c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-17 21:56:30 +0000

    Add tile-based collision for slimes

--- src/logic/ai/movement/FollowPlayer.lua
+++ src/logic/ai/movement/FollowPlayer.lua
@@ -5,14 +5,26 @@ local followPlayer = {}
 followPlayer.update = function(entity, game)
     local playerPos = game.player:toPosition()
     local entityPos = entity:toPosition()
+    local dx = 0
+    local dz = 0
+    local room = game.map:currentRoom()
 
     local dst = V.distance(entityPos, playerPos)
     -- TODO remove the hard-coded threshold below
     if dst > 1 then
         local unitV = V.normalise(entityPos, playerPos)
 
-        entity.x = entity.x + unitV.x * entity.speed * game.dt
-        entity.z = entity.z + unitV.z * entity.speed * game.dt
+        dx = unitV.x * entity.speed * game.dt
+        dz = unitV.z * entity.speed * game.dt
+
+        local newX = entity.x + dx
+        local newZ = entity.z + dz
+        local screenPos = V.worldToScreen({ x = newX, y = 0, z = newZ})
+
+        if room:walkable(screenPos.x, screenPos.y) then
+            entity.x = newX
+            entity.z = newZ
+        end
     end
 end
 
