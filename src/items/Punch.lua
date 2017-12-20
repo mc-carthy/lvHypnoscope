6a392420fb565419c8d01d4dd61854be2b0cfb28
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-20 23:31:41 +0000

    Create punch sfx

--- src/items/Punch.lua
+++ src/items/Punch.lua
@@ -4,9 +4,11 @@ local Sprite = require("src/graphics/Sprite")
 local punch = {}
 
 local punchSprite = Sprite.create("assets/sprites/punch.png")
+local punchSfx = love.audio.newSource("assets/sounds/punch.wav", "static")
 
 punch.create = function(x, y, z)
     local punch = Entity.create(punchSprite, x, y, z, 0)
+    love.audio.play(punchSfx)
 
     return punch
 end
